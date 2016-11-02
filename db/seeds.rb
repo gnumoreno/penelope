d50f6b1a7903238750f8a2b1275360e2586bff80
Moreno Garcia <gnumoreno@gmail.com>
2016-11-02 23:48:53 +0000

    Basic User Model with secure password

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!([
+  {name: "Moreno Garcia", email: "gnumoreno@gmail.com", password: "fooobaaar"},
+  {name: "Lucas Guimarães", email: "lokaster@gmail.com", password: "fooobaaar"},
+  {name: "Talles Tonatto", email: "talles.tonatto@gmail.com", password: "fooobaaar"},
+  {name: "Gabriel Knüpfer", email: "gabriel.knupfer0910@gmail.com", password: "fooobaaar"},
+  {name: "Gustavo Abadie", email: "gbabadie@gmail.com", password: "fooobaaar"},
+  {name: "Michael Hartl", email: "mhartl@example.com", password: "fooobaaar"}
+])
