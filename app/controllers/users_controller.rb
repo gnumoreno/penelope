f104b23761ece75835e9b980dd023ea0398cf11b
Moreno Garcia <gnumoreno@gmail.com>
2016-11-13 04:15:55 +0000

    Implemented basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to Penelope!"
       redirect_to @user
     else
@@ -14,6 +15,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
+  private
   
   def user_params
       params.require(:user).permit(:name, :email, :password,
