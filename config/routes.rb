cc60cab9176fe4a3ed4cbc23b8b24738b3363f4b
Moreno Garcia <gnumoreno@gmail.com>
2016-11-13 01:10:04 +0000

    Finished User Signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'users#new'
+  resources :users
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,13 @@
 User.create!([
-  {name: "Moreno Garcia", email: "gnumoreno@gmail.com", password: "fooobaaar"},
-  {name: "Lucas Guimarães", email: "lokaster@gmail.com", password: "fooobaaar"},
-  {name: "Talles Tonatto", email: "talles.tonatto@gmail.com", password: "fooobaaar"},
-  {name: "Gabriel Knüpfer", email: "gabriel.knupfer0910@gmail.com", password: "fooobaaar"},
-  {name: "Gustavo Abadie", email: "gbabadie@gmail.com", password: "fooobaaar"},
-  {name: "Michael Hartl", email: "mhartl@example.com", password: "fooobaaar"}
+  {name: "Moreno Garcia e Silva", email: "gnumoreno@gmail.com", password: "fooobaaar", password_confirmation: "fooobaaar"},
+  {name: "Lucas Guimarães", email: "lokaster@gmail.com", password: "fooobaaar", password_confirmation: "fooobaaar"},
+  {name: "Talles Tonatto", email: "talles.tonatto@gmail.com", password: "fooobaaar", password_confirmation: "fooobaaar"},
+  {name: "Gabriel Knüpfer", email: "gabriel.knupfer0910@gmail.com", password: "fooobaaar", password_confirmation: "fooobaaar"},
+  {name: "Gustavo Abadie", email: "gbabadie@gmail.com", password: "fooobaaar", password_confirmation: "fooobaaar"},
+  {name: "Michael Hartl", email: "mhartl@example.com", password: "fooobaaar", password_confirmation: "fooobaaar"},
+  {name: "User Example", email: "example@railstutorial.org", password: "fooobaaar", password_confirmation: "fooobaaar"},
+  {name: "Evandro Ferreira", email: "ealost@live.com", password: "fooobaaar", password_confirmation: "fooobaaar"},
+  {name: "Luciano Schühli", email: "luciano.sch@gmail.com", password: "fooobaaar", password_confirmation: "fooobaaar"},
+  {name: "Francisco Escorsim", email: "foescorsim@gmail.com", password: "fooobaaar", password_confirmation: "fooobaaar"},
+  {name: "Luis Caldas", email: "caldasluis30@gmail.com", password: "fooobaaar", password_confirmation: "fooobaaar"}
 ])
